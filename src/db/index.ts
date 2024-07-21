0278bddac0a8928c6151609246e901b31b70e161
sezeef <sezeef.h@gmail.com>
2024-07-21 13:10:37 +0300

    Removed unnecessary import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,5 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-
-config({ path: ".env.local" });
 
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
