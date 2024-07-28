e38e4e7c9119103a2132ef04a6cb1aef153cf5a4
sezeef <sezeef.h@gmail.com>
2024-07-28 19:21:56 +0300

    Impl util function for date-fns localization

--- src/lib/locale.ts
+++ src/lib/locale.ts
@@ -1,3 +1,6 @@
+import { enUS, ar } from "date-fns/locale";
+import { Locale as DateFnsLocale } from "date-fns";
+
 export const i18n = {
   defaultLocale: "en",
   locales: ["ar", "en"]
@@ -11,4 +14,14 @@ export function localize(l: string, r: string) {
 
 export function localizeRoutes(locales: string[], routes: string[]) {
   return locales.flatMap((l) => routes.map((r) => localize(l, r)));
+}
+
+export function getDateFnsLocale(locale: Locale): DateFnsLocale {
+  switch (locale) {
+    case "ar":
+      return ar;
+    case "en":
+    default:
+      return enUS;
+  }
 }
