aefd6b979a69e4bc4c4ec3a2e5ffe87c6b22d433
sezeef <sezeef.h@gmail.com>
2024-07-28 18:39:32 +0300

    Not using root page for now

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,26 +1,7 @@
-import LocaleSwitcher from "@/components/lang-switcher";
-import { auth } from "@/lib/auth";
-import { getDictionary } from "@/lib/get-dictionary";
-import { type Locale } from "@/lib/locale";
+import { redirect } from "next/navigation";
+import { safeGetUser } from "@/lib/user";
 
-export default async function Home({
-  params: { lang }
-}: {
-  params: { lang: Locale };
-}) {
-  const dictionary = await getDictionary(lang);
-  const session = await auth();
-  return (
-    <main>
-      <div>
-        <h2>Current langague: {lang}</h2>
-        <p>
-          This text is rendered on the server: {dictionary["/"]["body:welcome"]}
-        </p>
-        <p>{JSON.stringify(session?.user)}</p>
-      </div>
-      <br />
-      <LocaleSwitcher />
-    </main>
-  );
+export default async function HomePage() {
+  const user = await safeGetUser();
+  user ? redirect("/dashboard") : redirect("/auth/login");
 }
