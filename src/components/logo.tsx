d46eebd7aca8cb9ffde13081506b1920fb9f80bb
sezeef <sezeef.h@gmail.com>
2024-07-30 21:29:35 +0300

    Updated logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,17 @@
 import { Locale, localize } from "@/lib/locale";
+import Image from "next/image";
 import Link from "next/link";
 
 export function Logo({ lang, title }: { lang: Locale; title: string }) {
   return (
-    <Link
-      href={localize(lang, "/")}
-      className="font-bold text-3xl hover:cursor-pointer"
-    >
-      {title}
-    </Link>
+    <div className="flex gap-1 items-center">
+      <Image src="/images/Logo.svg" width={32} height={32} alt="Logo" />
+      <Link
+        href={localize(lang, "/")}
+        className="font-bold text-3xl hover:cursor-pointer"
+      >
+        {title}
+      </Link>
+    </div>
   );
 }
