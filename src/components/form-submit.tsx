a7fb0abe45705c6c2ec0315ef2ffc9f4ca77c6a1
sezeef <sezeef.h@gmail.com>
2024-07-31 11:31:21 +0300

    Updated style

--- src/components/form-submit.tsx
+++ src/components/form-submit.tsx
@@ -79,7 +79,7 @@ export function FormSubmit({
   if (submitted) {
     return (
       <div className="flex justify-center w-full h-full items-center p-8">
-        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
+        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border rounded">
           <h1 className="text-2xl font-bold">Form submitted</h1>
           <p className="text-muted-foreground">
             {dictionary.submit["message:thanks-for-submit"]}
@@ -93,7 +93,7 @@ export function FormSubmit({
     <div className="flex justify-center w-full h-full items-center p-8">
       <div
         key={renderKey}
-        className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded"
+        className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border rounded"
       >
         {content.map((element) => {
           const FormElement = formElements[element.type].formComponent;
