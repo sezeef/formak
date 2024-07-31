ea5bf30fd13d0bccba60d52a8cf50a8b86765a1a
sezeef <sezeef.h@gmail.com>
2024-07-31 12:27:28 +0300

    Added check to prevent publishing empty forms

--- src/actions/form/publish-form.ts
+++ src/actions/form/publish-form.ts
@@ -1,10 +1,24 @@
 "use server";
-import { publishFormById } from "@/db/query/form";
+import { getFormById, publishFormById } from "@/db/query/form";
 import { AppError, ERROR_CODES } from "@/lib/error";
 import { unsafeGetUser } from "@/lib/user";
 
 export async function publishForm(id: string) {
   const user = await unsafeGetUser();
+
+  const form = await getFormById({
+    userId: user.id,
+    formId: id
+  });
+
+  if (!form) {
+    throw new AppError(ERROR_CODES.SYS_DB_FAILURE);
+  }
+
+  if (!form.content) {
+    throw new AppError(ERROR_CODES.VAL_INVALID_FIELD);
+  }
+
   const result = await publishFormById({
     formId: id,
     userId: user.id
