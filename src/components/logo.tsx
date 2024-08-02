3a4ba4a3203e3b1359404a454f850eab32dadbbe
sezeef <sezeef.h@gmail.com>
2024-08-02 15:42:55 +0300

    Updated to have responsive text

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,24 @@
 import { Locale, localize } from "@/lib/locale";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export function Logo({ lang, title }: { lang: Locale; title: string }) {
+export function Logo({
+  lang,
+  title,
+  className
+}: {
+  lang: Locale;
+  title: string;
+  className?: string;
+}) {
   return (
     <Link
       href={localize(lang, "/")}
-      className="flex gap-1 items-center hover:cursor-pointer"
+      className={cn("flex gap-1 items-center hover:cursor-pointer", className)}
     >
-      <Image src="/images/Logo.svg" width={32} height={32} alt="Logo" />
-      <div className="font-robotoSlab font-medium text-3xl">{title}</div>
+      <Image src="/images/Logo.svg" width={32} height={32} alt="Logo" className="rounded" />
+      <div className="font-robotoSlab font-medium text-2xl lg:text-3xl">{title}</div>
     </Link>
   );
 }
