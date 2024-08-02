9ccac6dd7f4bfe7e528ddd723968b8456deddc07
sezeef <sezeef.h@gmail.com>
2024-08-02 19:59:46 +0300

    Fixed window is undefined error

--- src/components/form-link-share.tsx
+++ src/components/form-link-share.tsx
@@ -1,13 +1,24 @@
 "use client";
+import { useEffect, useState } from "react";
 import { ImShare } from "react-icons/im";
+
+import { useDictionary } from "@/components/dictionary-context";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
-import { useDictionary } from "./dictionary-context";
 
 export function FormLinkShare({ shareUrl }: { shareUrl: string }) {
   const { dictionary } = useDictionary();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   const url = `${window.location.origin}/submit/${shareUrl}`;
   return (
     <div className="flex flex-grow gap-4 items-center">
