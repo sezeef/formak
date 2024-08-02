c2c7dbc05cd62282de1dd147157c0c40b4da64e4
sezeef <sezeef.h@gmail.com>
2024-08-02 16:05:24 +0300

    Fixed bug - unexpected undefined result

--- src/actions/form/get-form-content.ts
+++ src/actions/form/get-form-content.ts
@@ -1,10 +1,11 @@
+import { eq, sql } from "drizzle-orm";
 import { getDb } from "@/db";
 import { formTable } from "@/db/schema/form";
-import { eq, sql } from "drizzle-orm";
+import { AppError, ERROR_CODES } from "@/lib/error";
 
 export async function getFormContentByUrl(formUrl: string) {
   const db = await getDb();
-  return await db
+  const form = await db
     .update(formTable)
     .set({
       visits: sql`${formTable.visits} + 1`
@@ -12,4 +13,10 @@ export async function getFormContentByUrl(formUrl: string) {
     .where(eq(formTable.shareUrl, formUrl))
     .returning({ content: formTable.content })
     .then((res) => res[0]);
+
+  if (!form) {
+    throw new AppError(ERROR_CODES.SYS_DB_FAILURE);
+  }
+
+  return form;
 }
